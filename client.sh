a1cb6d5d34d3184ba6ab1bfd48789e99457150ef
NimaBavari <nima.bavari@gmail.com>
2023-02-14 17:44:04 +0400

    Decoupled client from server

--- client.sh
+++ client.sh
@@ -6,4 +6,10 @@ then
     source simenv/bin/activate
 fi
 
+read -p "Enter output host (skip for localhost): " output_websocket_host
+[ -z "$output_websocket_host" ] && output_websocket_host="localhost"
+
+read -p "Enter output port (skip for 8767): " output_websocket_port
+[ -z "$output_websocket_port" ] && output_websocket_port=8767
+
 simenv/bin/python -m websockets "ws://$output_websocket_host:$output_websocket_port/"
